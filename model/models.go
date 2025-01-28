9aaa7fbc6ebd2e9a85ec5b6c64d9e82fedd7b81d
shfana <wambitafana@gmail.com>
2025-01-28 17:30:25 +0300

    Feat: add post struct

--- model/models.go
+++ model/models.go
@@ -9,8 +9,26 @@ import (
 // user struct
 type User struct {
 	ID        uuid.UUID `json:"id"`
-	Username  string    `json:"username" validate: "required, min=3"`
+	Username  string    `json:"username" validate: "required,min=3"`
 	Email     string    `json:"email" validate: "required, email"`
 	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+// post struct
+type Post struct {
+	ID         uuid.UUID `json:"id"`
+	UserID     uuid.UUID `json: "user_id"`
+	CategoryID uuid.UUID `json:"category_id"`
+	Title      string    `json:"title" validate:"required"`
+	Content    string    `json:"content" validate: "required"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	DeletedAt  time.Time `json: "deleted_at"`
+
+	// relationships
+	User     *User      `json:"user, omitempty"`
+	Category *Category  `json: "category, omitempty"`
+	Comments []*Comment `json: "comments, omitempty"`
+	Votes int 	`json:"votes"`
 }
