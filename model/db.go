58d1e2e3a7c5d5a2f3b402427c1d9c9e50fb7420
shfana <wambitafana@gmail.com>
2025-01-28 12:56:01 +0300

    Feat: add function to initilize db, add data source name to enabel foreign key constraints in sqlite, and write ahead logging for better concurrency, and open a connection to sqlite

--- model/db.go
+++ model/db.go
@@ -1,3 +1,25 @@
 package models
 
+import (
+	"database/sql"
+	"embed"
+	"fmt"
+)
+
 // Database connection and initialization.
+// go : embed migrations .sql
+var fs embed.FS
+
+const (
+	dbName = "forum.db"
+)
+
+func InitializeDB() (*sql.DB, error) {
+	// open database connection
+	// data source name
+	dsn := fmt.Sprintf("file:%s?_foreign_keys=on&_journal_mode=WAL", dbName)
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to open databas: %w", err)
+	}
+}
