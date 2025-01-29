a9b0ddb80547ccd5355bc1a7c813697c0d4c1db1
rayjonesjay <rayjaymuiruri@gmail.com>
2025-01-29 14:45:45 +0300

    fix: changed function names and improved the validate password function

--- model/user.go
+++ model/user.go
@@ -2,32 +2,70 @@ package models
 
 // Handles database interaction and business logic.
 import (
+	"errors"
 	"forum/xerrors"
-
 	"golang.org/x/crypto/bcrypt"
+	"unicode"
 )
 
 var (
-	Cost int = bcrypt.DefaultCost
+	Cost                  int = bcrypt.DefaultCost
+	MinimumPasswordLength int = 8
 )
 
-// IsValidPassword compares the password and hashedPassword and checks if they match, if not it returns False else True (meaning they match)
-func IsValidPassword(password, hashedPassword string) bool {
+// VerifyPassword compares the password and hashedPassword and checks if they match, if not it returns False else True (meaning they match)
+func VerifyPassword(password, hashedPassword string) bool {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)) == nil
 }
 
 // HashPassword attempts to hash password using Cost value and returns the hashed password and error will be nil if successful
 // else hashed password will be an empty string and error will be not nil
 func HashPassword(password string) (string, error) {
-	if len(password) > 72 {
-		return "", bcrypt.ErrPasswordTooLong
-	}
-	if len(password) < 8 {
+	if err := validatePassword(password); err != nil {
 		return "", xerrors.ErrPasswordTooShort
 	}
-	password_bytes, err := bcrypt.GenerateFromPassword([]byte(password), Cost)
+	passwordBytes, err := bcrypt.GenerateFromPassword([]byte(password), Cost)
 	if err != nil {
 		return "", err
 	}
-	return string(password_bytes), nil
+	return string(passwordBytes), nil
+}
+
+func validatePassword(password string) error {
+	if len(password) < MinimumPasswordLength {
+		return xerrors.ErrPasswordTooShort
+	}
+
+	var hasUpper, hasLower, hasNumber, hasPunct bool
+
+	for _, c := range password {
+		switch {
+		case unicode.IsUpper(c):
+			hasUpper = true
+		case unicode.IsLower(c):
+			hasLower = true
+		case unicode.IsNumber(c):
+			hasNumber = true
+		case unicode.IsPunct(c):
+			hasPunct = true
+		}
+	}
+
+	if !hasUpper {
+		return errors.New("password must contain at least one uppercase letter")
+	}
+
+	if !hasLower {
+		return errors.New("password must contain at least one lowercase letter")
+	}
+
+	if !hasNumber {
+		return errors.New("password must contain at least one number")
+	}
+
+	if !hasPunct {
+		return errors.New("password must contain at least one special character")
+	}
+
+	return nil
 }
