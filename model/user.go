ca67ae1dceeb4057a1cef89e5ea927737af1c8df
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-01 15:28:49 +0300

    refactor:update verifypassword to IsValidPassword

--- model/user.go
+++ model/user.go
@@ -4,8 +4,9 @@ package model
 import (
 	"errors"
 	"forum/xerrors"
-	"golang.org/x/crypto/bcrypt"
 	"unicode"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -14,7 +15,7 @@ var (
 )
 
 // VerifyPassword compares the password and hashedPassword and checks if they match, if not it returns False else True (meaning they match)
-func VerifyPassword(password, hashedPassword string) bool {
+func IsValidPassword(password, hashedPassword string) bool {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)) == nil
 }
 
