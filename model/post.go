c2ef38834bf69a0788d9b84553b323d81bf76cdb
wyonyango <wycliffealphus7@gmail.com>
2025-02-03 16:05:58 +0300

    feat:add retrieving post logic

--- model/post.go
+++ model/post.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+
+	"forum/xerrors"
 )
 
 // CreatePost creates a new post
@@ -33,4 +35,53 @@ func CreatePost(db *sql.DB, post *Post) error {
 	}
 
 	return tx.Commit()
+}
+
+func GetPostByID(db *sql.DB, postID uuid.UUID) (*Post, error) {
+	post := &Post{}
+
+	err := db.QueryRow(`
+		SELECT 
+			p.id, p.user_id, p.title, p.content, p.created_at, p.updated_at,
+			u.id, u.username,
+			c.id, c.name,
+			COALESCE(v.vote_count, 0) as votes
+		FROM posts p
+		LEFT JOIN users u ON p.user_id = u.id
+		LEFT JOIN categories c ON p.category_id = c.id
+		LEFT JOIN (
+			SELECT post_id, SUM(value) as vote_count 
+			FROM votes 
+			GROUP BY post_id
+		) v ON p.id = v.post_id
+		WHERE p.id = ?
+	`, postID).Scan(
+		&post.ID,
+		&post.UserID,
+		&post.Title,
+		&post.Content,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+		&post.User.ID,
+		&post.User.Username,
+		&post.Category.ID,
+		&post.Category.Name,
+		&post.Votes,
+	)
+
+	if err == sql.ErrNoRows {
+		return nil, xerrors.ErrInvalidPost
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	comments, err := GetCommentsByPostID(db, post.ID)
+	if err != nil {
+		return nil, err
+	}
+	post.Comments = comments
+
+	return post, nil
 }
