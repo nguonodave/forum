6d10422f35cf022fb1a7cd36b0df77335dc09665
wyonyango <wycliffealphus7@gmail.com>
2025-02-03 15:34:36 +0300

    add function adding post to DB

--- model/post.go
+++ model/post.go
@@ -1,2 +1,36 @@
 package model
-// # Post model: manages post data.
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// CreatePost creates a new post
+func CreatePost(db *sql.DB, post *Post) error {
+	if err := ValidatePost(post); err != nil {
+		return err
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	defer tx.Rollback()
+
+	post.ID = uuid.New()
+	post.CreatedAt = time.Now()
+	post.UpdatedAt = time.Now()
+
+	_, err = tx.Exec(`
+	INSERT INTO posts (id, user_id, title, content, created_at, updated_at, category_id)
+	VALUES (?, ?, ?, ?, ?, ?, ?)	
+	`, post.ID, post.UserID, post.Title, post.Content, post.CreatedAt, post.UpdatedAt, post.Category.ID)
+	if err != nil {
+		return err
+	}
+
+	return tx.Commit()
+}
