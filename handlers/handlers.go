8f3e59e19b0bc8c7e6fcb42a8c2d1c31b0830d5c
rayjonesjay <rayjaymuiruri@gmail.com>
2025-02-05 06:11:24 +0300

    added /regiger to handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"forum/controller"
 	"html/template"
 	"log"
@@ -15,8 +16,9 @@ var templatesDir = "view"
 // if method is POST it gets the values from the form and internally checks if details exist in the database
 func Login(w http.ResponseWriter, r *http.Request) {
 	templateFile := "auth/login.html"
-
+	fmt.Println("resp", *r)
 	if r.Method == "GET" {
+		println(2)
 		TemplateError := func(message string, err error) {
 			http.Error(w, "Internal Server Error!", http.StatusInternalServerError)
 			log.Printf("%s: %v", message, err)
@@ -32,6 +34,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	} else if r.Method == "POST" {
+		println(3)
 		// parse form and populate r.Form
 		err := r.ParseForm()
 		if err != nil {
@@ -41,14 +44,14 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 		email := r.Form.Get("email")
 		password := r.FormValue("password")
-		username := r.Form.Get("username")
-
-		sessionToken, expiresAt, err := controller.HandleLogin(email, password, username)
+		fmt.Println(email, password)
+		sessionToken, expiresAt, err := controller.HandleLogin(email, password)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+
 		http.SetCookie(w, &http.Cookie{
 			Name:     "session",
 			Value:    sessionToken,
@@ -60,6 +63,52 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 		http.Redirect(w, r, "/", http.StatusFound)
 	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+// Register handles /register endpoint for registering
+func Register(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodPost:
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		username := r.Form.Get("username")
+		password := r.Form.Get("password")
+		email := r.Form.Get("email")
+
+		err = controller.HandleRegister(username, email, password)
+		if err != nil {
+			http.Error(w, "error during registration", http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/login", http.StatusFound)
+
+	case http.MethodGet:
+		templateFile := "auth/login.html"
+
+		TemplateError := func(message string, err error) {
+			http.Error(w, "Internal Server Error!", http.StatusInternalServerError)
+			log.Printf("%s: %v", message, err)
+		}
+		temp, err := template.ParseFiles(filepath.Join(templatesDir, templateFile))
+		if err != nil {
+			TemplateError("error parsing template", err)
+			return
+		}
+		err = temp.Execute(w, struct{}{})
+		if err != nil {
+			TemplateError("error executing template", err)
+			return
+		}
+	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
