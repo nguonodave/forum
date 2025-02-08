6bea18f166c501ca497b6f30e30a567b05a854e0
wyonyango <wycliffealphus7@gmail.com>
2025-02-08 15:50:46 +0300

    test:add mock db

--- controller/auth_test.go
+++ controller/auth_test.go
@@ -7,10 +7,34 @@ type mockDB struct {
 	execFunc     func(query string, args ...interface{}) (sql.Result, error)
 	queryRowFunc func(query string, args ...interface{}) *sql.Row
 }
-
-type mockResult struct {
-	lastInsertedId int64
-	rowsAffected   int64
+type Result interface {
+	lastInsertId() (int64, error)
+	RowsAffected() (int64, error)
 }
 
+type mockResult struct {
+	lastInsertId int64
+	rowsAffected int64
+}
 
+func (m mockResult) LastInsertId() (int64, error) {
+	return m.lastInsertId, nil
+}
+
+func (m mockResult) RowsAffected() (int64, error) {
+	return m.rowsAffected, nil
+}
+
+func (m *mockDB) Exec(query string, args ...interface{}) (sql.Result, error) {
+	if m.execFunc != nil {
+		return m.execFunc(query, args...)
+	}
+	return mockResult{1, 1}, nil
+}
+
+func (m *mockDB) QueryRow(query string, args ...interface{}) *sql.Row {
+	if m.queryRowFunc != nil {
+		return m.queryRowFunc(query, args...)
+	}
+	return &sql.Row{}
+}
