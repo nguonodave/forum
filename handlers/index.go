0109db0dcea235bbac68b1cd6bde9661668b2c3e
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-09 18:12:14 +0300

    feat:remove commented code

--- handlers/index.go
+++ handlers/index.go
@@ -24,7 +24,6 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error!", http.StatusInternalServerError)
 		log.Printf("%s: %v", message, err)
 	}
-
 	temp, err := template.ParseFiles(filepath.Join(templatesDir, templateFile))
 	if err != nil {
 		TemplateError("error parsing template", err)
@@ -36,35 +35,5 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		TemplateError("error executing template", err)
 		return
 	}
-}
 
-//
-//func Base(w http.ResponseWriter, r *http.Request) {
-//	templateFile := "template.html"
-//	type Content struct {
-//		Message string
-//		Data    string
-//	}
-//
-//	content := Content{
-//		Message: "Some message to pass to template",
-//		Data:    "Some data to pass to template",
-//	}
-//
-//	TemplateError := func(message string, err error) {
-//		http.Error(w, "Internal Server Error!", http.StatusInternalServerError)
-//		log.Printf("%s: %v", message, err)
-//	}
-//
-//	temp, err := template.ParseFiles(filepath.Join(templatesDir, templateFile))
-//	if err != nil {
-//		TemplateError("error parsing template", err)
-//		return
-//	}
-//
-//	err = temp.Execute(w, content)
-//	if err != nil {
-//		TemplateError("error executing template", err)
-//		return
-//	}
-//}
+}
