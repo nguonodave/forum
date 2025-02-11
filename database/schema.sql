056bde57fa3052d74bda1e84c746559507ad5b95
wyonyango <wycliffealphus7@gmail.com>
2025-02-11 11:44:55 +0300

    feat:image url column to posts table

--- database/schema.sql
+++ database/schema.sql
@@ -13,6 +13,7 @@ CREATE TABLE IF NOT EXISTS posts (
     user_id TEXT NOT NULL,
     title TEXT NOT NULL,
     content TEXT NOT NULL,
+    image_url TEXT,  
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
