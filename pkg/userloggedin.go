e5da532a47fd49a6e29d3a19d55d8beb2381bbdc
David Ochiel <nguonodave@gmail.com>
2025-02-12 04:33:45 +0300

    feat: implement UserLoggedIn function to check user session validity

--- pkg/userloggedin.go
+++ pkg/userloggedin.go
@@ -3,13 +3,21 @@ package pkg
 import (
 	"database/sql"
 	"net/http"
+	"time"
 )
 
 func UserLoggedIn(r *http.Request, db *sql.DB) bool {
 	// get cookie from request
+	cookie, cookieErr := r.Cookie("session")
+	if cookieErr != nil {
+		return false
+	}
 
 	// check if cookie details are in sessions table
+	var userId string
+	var expiryDate time.Time
+	sessionQueryErr := db.QueryRow(`SELECT user_id, expires_at FROM sessions WHERE id = ?`, cookie.Value).Scan(&userId, &expiryDate)
 
-	// return true is so
-	return true
+	// if no err, meaning session is available, return true
+	return sessionQueryErr == nil && time.Now().Before(expiryDate)
 }
