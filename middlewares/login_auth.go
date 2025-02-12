ed1aa0ba2932879cdc87652a0aa342114fa4f152
David Ochiel <nguonodave@gmail.com>
2025-02-12 05:10:22 +0300

    feat: add RedirectIfLoggedIn and guide comments for implementation

--- middlewares/login_auth.go
+++ middlewares/login_auth.go
@@ -1,2 +1,21 @@
 package middlewares
 
+import (
+	"net/http"
+)
+
+func RedirectIfLoggedIn(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// use UserLoggedIn from pkg to check if user is logged in
+		// if so redirect to home
+
+		// if not logged in, proceed to the next handler, Login
+	}
+}
+
+if pkg.UserLoggedIn(r, ) {
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+	return
+}
+
+handler(w, r)
