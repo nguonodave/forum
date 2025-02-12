5153d982f0d1891c92cfb39f501be30b3885ea58
David Ochiel <nguonodave@gmail.com>
2025-02-12 06:23:22 +0300

    feat: add RedirectIfLoggedIn in HandleFunc for register

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 
 	http.HandleFunc("/", controller.ValidateSession(db, handlers.Index))
 	http.HandleFunc("/login", middlewares.RedirectIfLoggedIn(db.Db, handlers.Login(db)))
-	http.HandleFunc("/register", handlers.Register(db))
+	http.HandleFunc("/register", middlewares.RedirectIfLoggedIn(db.Db, handlers.Register(db)))
 	http.HandleFunc("/api/posts", handlers.GetPaginatedPostsHandler(db))
 	http.HandleFunc("/logout", handlers.Logout(db))
 
