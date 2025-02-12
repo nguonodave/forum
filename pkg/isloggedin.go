714e88426ac86f051346b0e6fe5e35a7709f8605
David Ochiel <nguonodave@gmail.com>
2025-02-12 04:12:21 +0300

    feat: add function 'UserLoggedIn' with guide comments for checking if user is logged in

--- pkg/isloggedin.go
+++ pkg/isloggedin.go
@@ -1,1 +1,15 @@
 package pkg
+
+import (
+	"database/sql"
+	"net/http"
+)
+
+func UserLoggedIn(r *http.Request, db *sql.DB) bool {
+	// get cookie from request
+
+	// check if cookie details are in sessions table
+
+	// return true is so
+	return true
+}
