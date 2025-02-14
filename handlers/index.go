8a366a658a91d4926ef1626cb0bdff01f4fcae5e
David Ochiel <nguonodave@gmail.com>
2025-02-15 02:39:55 +0300

    feat: add functionality for adding posts to database

--- handlers/index.go
+++ handlers/index.go
@@ -9,8 +9,11 @@ import (
 	"path/filepath"
 	"time"
 
+	"forum/database"
 	"forum/model"
 	"forum/pkg"
+
+	"github.com/google/uuid"
 )
 
 // Index handler designed for the application's index page
@@ -19,18 +22,6 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-
-	// fetch all categories
-
-	// if method is POST (filters)
-	// form value
-	// GetPost(db, value)
-	// if value not empty, SELECT from db WHERE value
-	// else SELECT (*)
-	// populate posts
-	// else if GET
-	// all posts
-	// populate posts
 
 	type Content struct {
 		Message      string
@@ -47,18 +38,40 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == http.MethodPost {
-		// 20 MB limit
-		maxSizeErr := r.ParseMultipartForm(20 << 20)
-		if maxSizeErr != nil {
-			http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		cookie, cookieErr := r.Cookie("session")
+		if cookieErr != nil {
+			log.Printf("Error getting session's cookie from request: %v\n", cookieErr)
+			http.Error(w, "An unexpected error occured. Please try again later", http.StatusInternalServerError)
 			return
 		}
 
+		// get the user id to populate in post details
+		var userId string
+		sessionQueryErr := database.Db.QueryRow("SELECT user_id FROM sessions WHERE token = ?", cookie.Value).Scan(&userId)
+		if sessionQueryErr != nil {
+			log.Printf("Error fetching session's user id from database: %v\n", sessionQueryErr)
+			http.Error(w, "An unexpected error occured. Please try again later", http.StatusInternalServerError)
+			return
+		}
+
+		// 20 MB limit
+		maxSizeErr := r.ParseMultipartForm(20 << 20)
+		if maxSizeErr != nil {
+			http.Error(w, "Unable to parse form. Make sure image size is not more than 20mb", http.StatusBadRequest)
+			return
+		}
+
+		// get the submitted form content details
+		title := r.FormValue("title")
+		content := r.FormValue("content")
 		image, header, formFileErr := r.FormFile("image")
+
 		var imagePath string
 		if formFileErr == nil {
 			defer image.Close()
+
 			path := "./static/images/uploads/posts"
+
 			// create the dir that will store the file
 			mkdirErr := os.MkdirAll(path, os.ModePerm)
 			if mkdirErr != nil {
@@ -66,9 +79,11 @@ func Index(w http.ResponseWriter, r *http.Request) {
 				http.Error(w, "Failed to save image", http.StatusInternalServerError)
 				return
 			}
+
 			// unique filename for the image using dates and file name
 			imageName := fmt.Sprintf("%d-%s", time.Now().Unix(), header.Filename)
 			imagePath = filepath.Join(path, imageName)
+
 			// create the file path
 			dst, err := os.Create(imagePath)
 			if err != nil {
@@ -77,6 +92,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 			defer dst.Close()
+
 			// copy the image from the form to the created path
 			_, err = io.Copy(dst, image)
 			if err != nil {
@@ -84,6 +100,16 @@ func Index(w http.ResponseWriter, r *http.Request) {
 				http.Error(w, "Failed to save image", http.StatusInternalServerError)
 				return
 			}
+		}
+
+		postId := uuid.New().String()
+		createdTime := time.Now()
+
+		_, insertPostErr := database.Db.Exec(`INSERT INTO posts (id, user_id, title, content, image_url, created_at) VALUES (?, ?, ?, ?, ?, ?)`, postId, userId, title, content, imagePath, createdTime)
+		if insertPostErr != nil {
+			log.Printf("Error inserting post to database: %v\n", insertPostErr)
+			http.Error(w, "Failed to add post", http.StatusInternalServerError)
+			return
 		}
 	}
 
