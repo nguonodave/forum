89c969fe1d748e885e56bb71c2ad9aa06d20c6d6
David Ochiel <nguonodave@gmail.com>
2025-02-17 19:13:26 +0300

    feat: implement handler for displaying posts by categories
    - Instead of query, we now use path to get the category id for filtering

--- handlers/posts/categoryposts.go
+++ handlers/posts/categoryposts.go
@@ -1,8 +1,15 @@
 package posts
 
 import (
-	"forum/handlers"
+	"database/sql"
+	"log"
 	"net/http"
+	"strings"
+	"time"
+
+	"forum/database"
+	"forum/handlers"
+	"forum/pkg"
 )
 
 func CategoryPosts(w http.ResponseWriter, r *http.Request) {
@@ -10,5 +17,91 @@ func CategoryPosts(w http.ResponseWriter, r *http.Request) {
 		handlers.ErrorPage(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 
-	// queriedCategoryId := r.URL.Query().Get("category")
+	queriedCategoryId := strings.TrimPrefix(r.URL.Path, "/posts/")
+
+	// rendering posts to template
+	type Post struct {
+		Id          string
+		Title       string
+		Content     string
+		ImagePath   string
+		CreatedTime time.Time
+	}
+
+	var posts []Post
+
+	postsQuery := `
+	SELECT DISTINCT p.id, p.title, p.content, p.image_url, p.created_at
+	FROM posts p
+	LEFT JOIN post_categories pc ON p.id = pc.post_id
+	WHERE ? = '' OR pc.category_id = ?
+	ORDER BY p.created_at DESC
+	`
+
+	rows, err := database.Db.Query(postsQuery, queriedCategoryId, queriedCategoryId)
+	if err != nil {
+		log.Printf("Error fetching posts: %v\n", err)
+		http.Error(w, "Failed to fetch posts", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	// populate the post struct
+	for rows.Next() {
+		var post Post
+		var imagePath sql.NullString // use sql.NullString to handle NULL values
+		err := rows.Scan(&post.Id, &post.Title, &post.Content, &imagePath, &post.CreatedTime)
+		if err != nil {
+			log.Printf("Error scanning post: %v\n", err)
+			continue
+		}
+
+		// if imagePath is valid, assign it to the post
+		if imagePath.Valid {
+			post.ImagePath = "../" + imagePath.String
+		}
+
+		posts = append(posts, post)
+	}
+
+	// fetch all categories to render to the create post form
+	categRows, categQueryErr := database.Db.Query(`SELECT id, name FROM categories`)
+	if categQueryErr != nil {
+		log.Printf("Error fetching categories: %v\n", categQueryErr)
+		http.Error(w, "Failed to fetch categories", http.StatusInternalServerError)
+		return
+	}
+	defer categRows.Close()
+
+	var categories []struct {
+		Id   string
+		Name string
+	}
+	for categRows.Next() {
+		var category struct {
+			Id   string
+			Name string
+		}
+		err := categRows.Scan(&category.Id, &category.Name)
+		if err != nil {
+			log.Printf("Error scanning category: %v\n", err)
+			continue
+		}
+		categories = append(categories, category)
+	}
+
+	TemplateError := func(message string, err error) {
+		http.Error(w, "Internal Server Error!", http.StatusInternalServerError)
+		log.Printf("%s: %v", message, err)
+	}
+
+	execTemplateErr := handlers.Templates.ExecuteTemplate(w, "categoryposts.html", map[string]interface{}{
+		"Posts":        posts,
+		"UserLoggedIn": pkg.UserLoggedIn(r),
+		"Categories":   categories,
+	})
+	if execTemplateErr != nil {
+		TemplateError("error executing template", execTemplateErr)
+		return
+	}
 }
