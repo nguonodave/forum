53fbca67967b67c9402c5ef45c60b2affcf0fa2e
David Ochiel <nguonodave@gmail.com>
2025-02-19 18:14:42 +0300

    feat: add function for getting categories from DB
    - Replaced the functionality in CategoryPosts for getting categories with the new function GetCategories.

---
+++ pkg/getcategories.go
@@ -0,0 +1,34 @@
+package pkg
+import (
+	"log"
+	"net/http"
+	"forum/database"
+)
+type category struct {
+	Id   string
+	Name string
+}
+func GetCategories(w http.ResponseWriter) ([]category, error) {
+	// fetch all categories to render to the create post form
+	categRows, categQueryErr := database.Db.Query(`SELECT id, name FROM categories`)
+	if categQueryErr != nil {
+		log.Printf("Error fetching categories: %v\n", categQueryErr)
+		http.Error(w, "Failed to fetch categories", http.StatusInternalServerError)
+		return nil, categQueryErr
+	}
+	defer categRows.Close()
+	var categories []category
+	for categRows.Next() {
+		var category struct {
+			Id   string
+			Name string
+		}
+		err := categRows.Scan(&category.Id, &category.Name)
+		if err != nil {
+			log.Printf("Error scanning category: %v\n", err)
+			continue
+		}
+		categories = append(categories, category)
+	}
+	return categories, nil
+}
