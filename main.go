67b387f939cf75d9845d861dd3511993f022a5c6
rayjonesjay <rayjaymuiruri@gmail.com>
2025-05-08 15:35:59 +0300

    refactor backend

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"real_time_forum/backend/db/controllers"
@@ -13,9 +15,12 @@ func init() {
 	controllers.CreateTables()
 }
 func main() {
+	var port = 8080
+	flag.IntVar(&port, "port", port, "port to listen on")
+	flag.Parse()
+
 	file := "frontend/templates/index.html"
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer((http.Dir("frontend/static")))))
-
 	http.HandleFunc("/login", handlers.HandleLogin)
 	http.HandleFunc("/signup", handlers.SignupHandler)
 	http.HandleFunc("/home", handlers.LoadAll)
@@ -31,6 +36,8 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, file)
 	})
-	log.Println("Server running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	addr := fmt.Sprintf(":%d", port)
+	log.Println("Server running on port", port)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
