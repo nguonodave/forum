32b3c98652aedad6124f52a0cb68187aec403e37
shfana <wambitafana@gmail.com>
2025-05-11 21:17:01 +0300

    feat: add login form

---
+++ frontend/static/js/pageContext/login.js
@@ -0,0 +1,11 @@
+export const signinContext = `
+    <div class="Form-div">
+        <form class="loggin siggnup" id="loginForm">
+            <label>Login</label><br>
+            <input type="text" class="input" placeholder="email/username" name="email" required><br>
+            <input type="password" class="input" placeholder="password" name="password" required><br>
+            <input type="submit" value="Login" id="form-loginBtn">
+        </form>
+        <p >Dont Have an account? <button id="signUpBtn">signup</button> </p> 
+    </div>
+` 
